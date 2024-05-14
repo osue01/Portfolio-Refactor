93521969c414d60d4c8642418aaaa35b181f57dc
Olivia <osue01@gmail.com>
2024-05-14 07:48:15 -1000

    dynamic port

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+const PORT = process.env.PORT || 3000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: PORT,
     open: true,
   },
 });
